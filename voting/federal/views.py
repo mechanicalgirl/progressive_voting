86b93284303333b896aa1a6475ad14ad6fdfefdf
mechanicalgirl <barbara.shaurette@gmail.com>
2017-05-11 14:57:17 -0500

    disable for prod for now

--- voting/federal/views.py
+++ voting/federal/views.py
@@ -1,13 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import District, Candidate, VoterRegistration
 
 def home(request):
-    d = District.objects.all()
-    context = {
-        'districts': d,
-    }
-    return render(request, 'federal/index.html', context)
+    print(request.build_absolute_uri())
+    if request.build_absolute_uri() == 'http://127.0.0.1:8000/':
+        d = District.objects.all()
+        context = {
+            'districts': d,
+        }
+        return render(request, 'federal/index.html', context)
+    return HttpResponse('')
 
 def by_district(request, district):
     state, dist = district.split('-')

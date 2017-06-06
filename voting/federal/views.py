3ce73114553badb07afbb573c69120d6dacf4e43
mechanicalgirl <barbara.shaurette@gmail.com>
2017-06-06 14:39:24 -0500

    neaten up the preview pages

--- voting/federal/views.py
+++ voting/federal/views.py
@@ -2,12 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import District, Candidate, VoterRegistration
+from .choices import STATE_CHOICES
 
 def home(request):
     if request.build_absolute_uri() == 'http://127.0.0.1:8000/' or request.path == '/preview/':
         d = District.objects.all()
+        district_groups = []
+        for state in STATE_CHOICES:
+            abbrev, label = state[0], state[1]
+            districts_by_state = District.objects.filter(state=abbrev)
+            g = {'state_label': label, 'districts': districts_by_state}
+            district_groups.append(g)
         context = {
-            'districts': d,
+            'district_groups': district_groups,
         }
         return render(request, 'federal/index.html', context)
     return HttpResponse('')

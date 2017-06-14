8e32018689565d1fe83f8a2840b828862451fb0c
Barbara Shaurette <barbarashaurette@Barbara-Shaurette-MacBook-Pro.local>
2017-06-13 21:55:21 -0400

    update more candidate ids

--- voting/federal/urls.py
+++ voting/federal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$',                             views.home,           name='home'),
     url(r'^preview/$',                     views.home,           name='home_preview'),
     url(r'^district/(?P<district>.+)/$',   views.by_district,    name='by_district'),
+    # url(r'^get_ids/$',                     views.get_ids,        name='get_ids'),
 ]
--- voting/federal/views.py
+++ voting/federal/views.py
@@ -1,3 +1,6 @@
+import sys
+import requests
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -46,3 +49,33 @@ def by_district(request, district):
         'candidates': ic,
     }
     return render(request, 'federal/district.html', context)
+
+def get_ids(request):
+    api_key = 'voQZ63Obcja8S2338pgGANCeSYmPMV0akuE49hS4'
+    base_url = 'https://api.open.fec.gov/v1'
+    endpoint = '/candidates/search/'
+    election_year = '2018' # make this a sys arg?
+
+    """
+    https://api.open.fec.gov/v1/candidates/search/?api_key=voQZ63Obcja8S2338pgGANCeSYmPMV0akuE49hS4&name=??
+    """
+    # SELECT name FROM federal_candidate WHERE candidate_id IS NULL ORDER BY name;
+    candidates = Candidate.objects.filter(candidate_id=None, active=True, incumbent=True)
+    for c in candidates:
+        name = c.name
+        # url = "%s%s?api_key=%s&name=%s&candidate_status=C" % (base_url, endpoint, api_key, name)
+        url = "%s%s?api_key=%s&name=%s" % (base_url, endpoint, api_key, name)
+        r = requests.get(url)
+        if r.status_code == 200:
+            results = r.json()['results']
+            if results:
+                candidate = parse_candidate(results, name)
+            else:
+                print("URL", url)
+    return HttpResponse('')
+
+def parse_candidate(results, name):
+    for candidate in results:
+        candidate_id = candidate['candidate_id']
+        print("UPDATE federal_candidate SET candidate_id = '%s' WHERE name = '%s';" % (candidate_id, name))
+
